c087ac8a6bf655184b32994a9607a259232adb80
Arm <arm.fullstack007@gmail.com>
2024-08-03 14:58:22 +0700

    Initial commit

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local lazypath = vim.fn.stdpath("data") .. "\\lazy\\lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "main",
+    "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
